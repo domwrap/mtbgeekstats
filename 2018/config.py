325a3d7eea61662c2ddd1f41a075306482641884
Hwulex <hwulex@gmail.com>
2018-06-03 23:06:36 -0700

    External config file

---
+++ 2018/config.py
@@ -0,0 +1,65 @@
+races = {
+    1: {
+        'name': 'losinj',
+        'title': 'Lošinj',
+        'loc': 'Croatia',
+        'urls': {
+            'uci': 'http://prod.chronorace.be/api/results/uci/dh/race/20180421_dh/',
+            'rnr': 'https://www.rootsandrain.com/race5897/2018-apr-22-mercedes-benz-uci-world-cup-1-losinj/results/filters/'
+        }
+    },
+    2: {
+        'name': 'fortbill',
+        'title': 'Fort William',
+        'loc': 'Scotland',
+        'urls': {
+            'uci': 'http://prod.chronorace.be/api/results/uci/dh/race/20180602_dh/',
+            'rnr': 'https://www.rootsandrain.com/race5898/2018-jun-3-mercedes-benz-uci-world-cup-2-fort-william/results/filters/'
+        }
+    },
+    3: {
+        'name': 'leogang',
+        'title': 'Leogang',
+        'loc': 'Austria',
+        'urls': {
+            'uci': '',
+            'rnr': ''
+        }
+    },
+    4: {
+        'name': 'valdisole',
+        'title': 'Val di Sole',
+        'loc': 'Italy',
+        'urls': {
+            'uci': '',
+            'rnr': ''
+        }
+    },
+    5: {
+        'name': 'vallnord',
+        'title': 'Vallnord',
+        'loc': 'Andorra',
+        'urls': {
+            'uci': '',
+            'rnr': ''
+        }
+    },
+    6: {
+        'name': 'msa',
+        'title': 'Mont Sainte Anne',
+        'loc': 'Scotland',
+        'urls': {
+            'uci': '',
+            'rnr': ''
+        }
+    },
+    7: {
+        'name': 'labresse',
+        'title': 'La Bresse',
+        'loc': 'France',
+        'urls': {
+            'uci': '',
+            'rnr': ''
+        }
+    },
+}
