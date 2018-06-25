cd83e55ee0326a120e8b6c81c16f834454d68f5b
Hwulex <hwulex@gmail.com>
2018-06-24 22:14:45 -0700

    Season analysis beginnings, and other forgotten changes

--- 2018/config.py
+++ 2018/config.py
@@ -3,74 +3,88 @@
 # https://dataride.uci.ch/iframe/CompetitionResults/51869/7/
 
 races = {
-    1: {
-        'name': 'losinj',
-        'title': 'Lošinj',
-        'loc': 'Croatia',
-        'date': '20180422',
-        'urls': {
-            'uci': 'http://prod.chronorace.be/api/results/uci/dh/race/20180421_dh/',
-            'rnr': 'https://www.rootsandrain.com/race5897/2018-apr-22-mercedes-benz-uci-world-cup-1-losinj/results/filters/'
+    2017: {
+        3: {
+            'name': 'leogang',
+            'title': 'Leogang',
+            'loc': 'Austria',
+            'date': '20170610',
+            'urls': {
+                'uci': 'http://prod.chronorace.be/api/results/uci/dh/race/20170610_leog/',
+                'rnr': 'https://www.rootsandrain.com/race4490/2017-jun-11-uci-world-cup-3-leogang/results/filters/'
+            }
         }
     },
-    2: {
-        'name': 'fortbill',
-        'title': 'Fort William',
-        'loc': 'Scotland',
-        'date': '20180603',
-        'urls': {
-            'uci': 'http://prod.chronorace.be/api/results/uci/dh/race/20180602_dh/',
-            'rnr': 'https://www.rootsandrain.com/race5898/2018-jun-3-mercedes-benz-uci-world-cup-2-fort-william/results/filters/'
+    2018: {
+        1: {
+            'name': 'losinj',
+            'title': 'Lošinj',
+            'loc': 'Croatia',
+            'date': '20180422',
+            'urls': {
+                'uci': 'http://prod.chronorace.be/api/results/uci/dh/race/20180421_dh/',
+                'rnr': 'https://www.rootsandrain.com/race5897/2018-apr-22-mercedes-benz-uci-world-cup-1-losinj/results/filters/'
+            }
+        },
+        2: {
+            'name': 'fortbill',
+            'title': 'Fort William',
+            'loc': 'Scotland',
+            'date': '20180603',
+            'urls': {
+                'uci': 'http://prod.chronorace.be/api/results/uci/dh/race/20180602_dh/',
+                'rnr': 'https://www.rootsandrain.com/race5898/2018-jun-3-mercedes-benz-uci-world-cup-2-fort-william/results/filters/'
+            }
+        },
+        3: {
+            'name': 'leogang',
+            'title': 'Leogang',
+            'loc': 'Austria',
+            'date': '20180610',
+            'urls': {
+                'uci': 'http://prod.chronorace.be/api/results/uci/dh/race/20180609_dh/',
+                'rnr': 'https://www.rootsandrain.com/race5899/2018-jun-10-mercedes-benz-uci-world-cup-3-leogang/results/filters/'
+            }
+        },
+        4: {
+            'name': 'valdisole',
+            'title': 'Val di Sole',
+            'loc': 'Italy',
+            'date': '20180708',
+            'urls': {
+                'uci': '',
+                'rnr': ''
+            }
+        },
+        5: {
+            'name': 'vallnord',
+            'title': 'Vallnord',
+            'loc': 'Andorra',
+            'date': '20180715',
+            'urls': {
+                'uci': '',
+                'rnr': ''
+            }
+        },
+        6: {
+            'name': 'msa',
+            'title': 'Mont Sainte Anne',
+            'loc': 'Canada',
+            'date': '20180812',
+            'urls': {
+                'uci': '',
+                'rnr': ''
+            }
+        },
+        7: {
+            'name': 'labresse',
+            'title': 'La Bresse',
+            'loc': 'France',
+            'date': '20180826',
+            'urls': {
+                'uci': '',
+                'rnr': ''
+            }
         }
-    },
-    3: {
-        'name': 'leogang',
-        'title': 'Leogang',
-        'loc': 'Austria',
-        'date': '20180610',
-        'urls': {
-            'uci': 'http://prod.chronorace.be/api/results/uci/dh/race/20180609_dh/',
-            'rnr': 'https://www.rootsandrain.com/race5899/2018-jun-10-mercedes-benz-uci-world-cup-3-leogang/results/filters/'
-        }
-    },
-    4: {
-        'name': 'valdisole',
-        'title': 'Val di Sole',
-        'loc': 'Italy',
-        'date': '20180708',
-        'urls': {
-            'uci': '',
-            'rnr': ''
-        }
-    },
-    5: {
-        'name': 'vallnord',
-        'title': 'Vallnord',
-        'loc': 'Andorra',
-        'date': '20180715',
-        'urls': {
-            'uci': '',
-            'rnr': ''
-        }
-    },
-    6: {
-        'name': 'msa',
-        'title': 'Mont Sainte Anne',
-        'loc': 'Canada',
-        'date': '20180812',
-        'urls': {
-            'uci': '',
-            'rnr': ''
-        }
-    },
-    7: {
-        'name': 'labresse',
-        'title': 'La Bresse',
-        'loc': 'France',
-        'date': '20180826',
-        'urls': {
-            'uci': '',
-            'rnr': ''
-        }
-    },
+    }
 }
